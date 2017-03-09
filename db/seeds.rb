c923d56e0e0ad9e0f56a69dec0db6538b17386fb
Kylee Acker <kyleeacker@Kylees-MacBook-Air.local>
2017-03-09 16:46:51 -0500

    App

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  book = Book.new(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    created_at: Faker::Time.between(2.days.ago, Time.now),
+    updated_at: Faker::Time.between(1.days.ago, Time.now)
+  )
+  if book.valid?
+    book.save!
+  end
+end
